5c86da1da1d96bc9a17ccaaab180f566c780fd06
vlasov_37 <g10k.info@gmail.com>
2024-10-05 20:11:16 +0300

    add solution to `02-basics-2/30-calculator`

--- 02-basics-2/30-calculator/CalculatorApp.js
+++ 02-basics-2/30-calculator/CalculatorApp.js
@@ -1,26 +1,45 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
 
 export default defineComponent({
   name: 'CalculatorApp',
 
-  setup() {},
+  setup: function () {
+    let a = ref(0)
+    let b = ref(0)
+    let operation = ref('sum')
+
+    const result = computed(() => {
+      if (operation.value === 'sum') {
+        return a.value + b.value
+      } else if (operation.value === 'subtract') {
+        return a.value - b.value
+      } else if (operation.value === 'multiply') {
+        return a.value * b.value
+      } else if (operation.value === 'divide') {
+        return a.value / b.value
+      }
+    })
+
+    return {
+      a,
+      b,
+      result,
+      operation,
+    }
+  },
 
   template: `
     <div class="calculator">
-      <input type="number" aria-label="First operand" />
-
+      <input type="number" aria-label="First operand" v-model="a"/>
       <div class="calculator__operators">
-        <label><input type="radio" name="operator" value="sum"/>➕</label>
-        <label><input type="radio" name="operator" value="subtract"/>➖</label>
-        <label><input type="radio" name="operator" value="multiply"/>✖</label>
-        <label><input type="radio" name="operator" value="divide"/>➗</label>
+        <label><input type="radio" name="operator" v-model="operation" value="sum"/>➕</label>
+        <label><input type="radio" name="operator" v-model="operation" value="subtract"/>➖</label>
+        <label><input type="radio" name="operator" v-model="operation" value="multiply"/>✖</label>
+        <label><input type="radio" name="operator" v-model="operation" value="divide"/>➗</label>
       </div>
-
-      <input type="number" aria-label="Second operand" />
-
+      <input type="number" aria-label="Second operand" v-model="b"/>
       <div>=</div>
-
-      <output>0</output>
+      <output>{{result}}</output>
     </div>
   `,
 })

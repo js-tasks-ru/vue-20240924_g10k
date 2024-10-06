0d052bf247ed7b698999cbcc0f9dae1af0a2770f
vlasov_37 <g10k.info@gmail.com>
2024-10-06 15:21:41 +0300

    add solution to `03-components/10-MeetupView`

--- 03-components/10-MeetupView/MeetupView.js
+++ 03-components/10-MeetupView/MeetupView.js
@@ -5,38 +5,45 @@ import MeetupDescription from './MeetupDescription.js'
 import MeetupCover from './MeetupCover.js'
 import MeetupInfo from './MeetupInfo.js'
 import './MeetupView.css'
+// import MeetupDescription from "@/MeetupDescription.js";
 
 export default defineComponent({
   name: 'MeetupView',
 
+  props: {
+    meetup: {
+      type: Object,
+      required: true,
+      // как указать, что 1) ожидается agenda 2) ее с length > 1
+    }
+  },
   components: {
+    MeetupDescription,
+    MeetupCover,
+    MeetupInfo,
+    MeetupAgenda,
     UiAlert,
     UiContainer,
   },
 
   template: `
     <div>
-
       <!-- Обложка митапа -->
-
+      <MeetupCover :title="meetup.title" :image="meetup.image"></MeetupCover>
       <UiContainer>
         <div class="meetup">
           <div class="meetup__content">
             <h2>Описание</h2>
-
-            <!-- Описание митапа -->
-
+            <MeetupDescription :description="meetup.description"/>
             <h2>Программа</h2>
-
             <!-- Программа митапа -->
             <!-- Или при пустой программе - сообщение "Программа пока пуста..." в UiAlert -->
-            <UiAlert></UiAlert>
-
+            <MeetupAgenda v-if="meetup &&  meetup.agenda.length" :agenda="meetup.agenda"></MeetupAgenda>
+            <UiAlert v-else>Программа пока пуста...</UiAlert>
           </div>
           <div class="meetup__aside">
-
             <!-- Краткая информация о митапе -->
-
+            <MeetupInfo :organizer="meetup.organizer" :place="meetup.place" :date="meetup.date"/>
             <div class="meetup__aside-buttons"></div>
           </div>
         </div>
